02ae326cb48ccf49f5e5d4116727be27eb0ad6d1
devjosegomez <devjosegomez@gmail.com>
2019-10-14 15:39:28 -0500

    2nd way to generate random numbers

--- src/com/exercise7arrays/app/RandomNumbers.java
+++ src/com/exercise7arrays/app/RandomNumbers.java
@@ -5,15 +5,13 @@ import java.util.Random;
 public class RandomNumbers {
 	final int ELEMENTS_ARRAY = 10;
 	
-	int[] myArray1 = new int[ELEMENTS_ARRAY];
-	double generatedNumber = 0;
-	int generatedNumberInt=0;
-	
-	
-	//Array pseudo random numbers
-	//1st way with Random class
-	Random randomNumbers = new Random(System.nanoTime());
-	
 	//2nd way with Math class
+	double generatedNumber = 0;
+	int generatedNumberInt = 0;
+	
+	for(int i=0; i<ELEMENTS_ARRAY; i++) {
+		generatedNumber = (int)(Math.random()*50+50);
+		System.out.println(generatedNumber);
+	}
 	
 }
