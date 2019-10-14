d82b90c70f7a68c5d1cd2393001c78388a8b71ca
devjosegomez <devjosegomez@gmail.com>
2019-10-14 15:11:05 -0500

    added 1st way of generating a pseudo random number

--- src/com/exercise7arrays/app/RandomNumbers.java
+++ src/com/exercise7arrays/app/RandomNumbers.java
@@ -6,10 +6,13 @@ public class RandomNumbers {
 	final int ELEMENTS_ARRAY = 10;
 	
 	int[] myArray1 = new int[ELEMENTS_ARRAY];
+	double generatedNumber = 0;
+	int generatedNumberInt=0;
+	
 	
 	//Array pseudo random numbers
 	//1st way with Random class
-	Random randomNumbers = new Random();
+	Random randomNumbers = new Random(System.nanoTime());
 	
 	//2nd way with Math class
 	
