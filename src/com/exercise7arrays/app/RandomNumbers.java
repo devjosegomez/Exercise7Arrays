e9b929d000f9cce871b34aae3e064a2a2590391c
devjosegomez <devjosegomez@gmail.com>
2019-10-14 14:52:59 -0500

    Added class RandomNumers

---
+++ src/com/exercise7arrays/app/RandomNumbers.java
@@ -0,0 +1,16 @@
+package com.exercise7arrays.app;
+
+import java.util.Random;
+
+public class RandomNumbers {
+	final int ELEMENTS_ARRAY = 10;
+	
+	int[] myArray1 = new int[ELEMENTS_ARRAY];
+	
+	//Array pseudo random numbers
+	//1st way with Random class
+	Random randomNumbers = new Random();
+	
+	//2nd way with Math class
+	
+}
